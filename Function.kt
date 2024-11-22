eb65d46ad269e0491ba9f7dac96f68944b1e3db8
RAHMANI  SAID <142494944+rahmanaboussaidy@users.noreply.github.com>
2024-11-22 23:05:15 +0300

    Add files via upload

---
+++ Function.kt
@@ -0,0 +1,30 @@
+// function description
+fun main(){
+    happybirthday()
+    val bday = birthdayGreeting()
+    println(bday)
+    details("juma", 21, "22,ikuti")
+}
+
+fun happybirthday(){
+    println("Happy Birthday John")
+    println("you are now 21 years old")
+}
+
+
+//return values from function
+
+fun birthdayGreeting(): String {
+    val nameGreeting = "Happy Birthday, Godfrey!"
+    val ageGreeting = "You are now 21 years old!"
+    return "$nameGreeting\n$ageGreeting"
+}
+
+//parameter declaration
+
+fun details(name: String, age: Int,address: String){
+    println("hello, my name is $name im $age years old and my address is $address") 
+}
+
+//function signature
+fun details(name: String, age: Int,address: String)
